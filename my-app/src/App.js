32ed8e6ab3418806bf4bb91957717a0acaf87f90
koztas <kiasinskiy@gmail.com>
2023-09-07 20:26:51 +0300

    init project

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+	return (
+		// декларативный стиль в разметке
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<p>This functional named 'Hot reload'.</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>{new Date().getFullYear()}</p>
+			</header>
+		</div>
+	);
+};
---
+++ my-app/src/AppWithoutJSX.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+export const AppWithoutJSX = () => {
+	return (
+		/*#__PURE__*/
+		// декларативный стиль в разметке
+		React.createElement(
+			'div',
+			{
+				className: 'App',
+			},
+			/*#__PURE__*/ React.createElement(
+				'header',
+				{
+					className: 'App-header',
+				},
+				/*#__PURE__*/ React.createElement('img', {
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				}),
+				/*#__PURE__*/ React.createElement(
+					'p',
+					null,
+					'Edit ',
+					/*#__PURE__*/ React.createElement('code', null, 'src/App.js'),
+					' and save to reload.',
+				),
+				/*#__PURE__*/ React.createElement(
+					'p',
+					null,
+					"This functional named 'Hot reload'.",
+				),
+				/*#__PURE__*/ React.createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				/*#__PURE__*/ React.createElement('p', null, new Date().getFullYear()),
+			),
+		)
+	);
+};
